8e46c4da187c0cf790ce12400a84856b581570df
Muhammad Sami Ullah <185323206+Muhammad-Sami-Ullah53@users.noreply.github.com>
2026-07-08 20:58:59 +0500

    Time: 0 ms (100%), Space: 8 MB (11.98%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
+        int ans=1;
         for(int i=0; i<31; i++){
-            if(pow(2,i)==n) return true;
+           if(ans==n) return true;
+           if(ans<INT_MAX/2)
+           ans*=2;
+            }
             
-        }
         return false;
     }
 };
