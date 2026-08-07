be56f0df4e73e444549f25c65c51b3777573c4f5
Muhammad Sami Ullah <185323206+Muhammad-Sami-Ullah53@users.noreply.github.com>
2026-08-07 11:38:11 +0500

    Time: 0 ms (100%), Space: 11.4 MB (33.86%) - LeetHub

--- 1051-height-checker/1051-height-checker.cpp
+++ 1051-height-checker/1051-height-checker.cpp
@@ -5,8 +5,8 @@ public:
         int size=heights.size();
         bool swaped=false;
         vector<int> arr=heights;
-        for(int i=1; i<size; i++){
-            for(int j=0; j<size-i; j++){
+        for(int i=0; i<size-1; i++){
+            for(int j=0; j<size-i-1; j++){
                 if(heights[j]>heights[j+1]){
                     swap(heights[j],heights[j+1]);
                     swaped=true;
