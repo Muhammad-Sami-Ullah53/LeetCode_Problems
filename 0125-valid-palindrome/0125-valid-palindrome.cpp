f532bb39f9bb7daa3c470703a631d1efb9671b23
M Sami Ullah Sarfraz <185323206+Muhammad-Sami-Ullah53@users.noreply.github.com>
2026-08-18 06:27:33 +0500

    Time: 0 ms (100%), Space: 10 MB (35.96%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
-char to_lower(char c){
-    if(c>='a' && c<='z'){
-        return c;
-    }
-    else {
-c=c-'A'+'a';
-return c;
-    }
-    
-}
-    bool isPalindrome(string str) {
-        int n=str.size();
-        int s=0;
-        int e=n-1;
-        while(s<e){
-             if (!isalnum(str[s])) {
-                s++;
+    bool isPalindrome(string s) {
+        int start = 0;
+        int end = s.size() - 1;
+
+        while (start < end) {
+
+            if (!isalnum(s[start])) {
+                start++;
                 continue;
             }
 
-            // Skip special characters from right
-           else if (!isalnum(str[e])) {
-                e--;
+            if (!isalnum(s[end])) {
+                end--;
                 continue;
-           }
-           else if(to_lower(str[s])!=to_lower(str[e])){
-                return 0;
             }
-           
-           s++;
-           e--;
+
+            if (tolower(s[start]) != tolower(s[end])) {
+                return false;
+            }
+
+            start++;
+            end--;
         }
-        return 1;
+
+        return true;
     }
 };
